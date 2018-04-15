e782e162e1b857f5831b12374286b623fbe54eba
liujh <liujh@knownsec.com>
2018-04-15 22:02:45 +0800

    chore: add prefix to static files

--- stencil.config.js
+++ stencil.config.js
@@ -1,7 +1,13 @@
 exports.config = {
   namespace: 'performance-to-video',
   globalStyle: 'src/global/style.css',
-  outputTargets: [{ type: 'dist' }, { type: 'www' }],
+  outputTargets: [
+    { type: 'dist' },
+    {
+      type: 'www',
+      resourcesUrl: '/perf2video/build/performance-to-video',
+    },
+  ],
 };
 
 exports.devServer = {
